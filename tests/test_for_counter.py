8d0e0b65846269518b27fd04d069f59abbaf32a9
Troy Tolhurst <troytolhurst@outlook.com>
2023-11-14 13:34:37 +0000

    Counter class fully tested

--- tests/test_for_counter.py
+++ tests/test_for_counter.py
@@ -1,1 +1,19 @@
 from lib.counter import * 
+def test_for_zero():
+    counter = Counter()
+    assert counter.report() == "Counted to 0 so far."
+
+def test_for_ten():
+    counter = Counter()
+    counter.add(10)
+    assert counter.report() == 'Counted to 10 so far.'
+
+def test_for_multiple():
+    counter = Counter()
+    counter.add(1)
+    counter.add(2)
+    counter.add(5)
+    counter.add(10)
+    counter.add(13)
+    counter.add(28)
+    assert counter.report() == 'Counted to 59 so far.'
